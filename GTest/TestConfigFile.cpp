bef2ed93dce12bfc15ce6e2026353e227e1b5ac6
matteo <matt.simonetti@hotmail.com>
2019-05-11 11:17:23 +0200

    Added more GoogleTests

--- GTest/TestConfigFile.cpp
+++ GTest/TestConfigFile.cpp
@@ -6,6 +6,33 @@
 
 TEST(ConfigFile,DefaultConstructor)
 {
+    bool alreadyExisted=std::filesystem::exists("config.ini");
     ConfigFile configFile;
+
+    ASSERT_TRUE(std::filesystem::exists("config.ini"));
+
     ASSERT_TRUE(configFile.GetKey("PawnccLocation")=="pawncc");
+
+    if(!alreadyExisted)
+        std::filesystem::remove("config.ini");
+}
+
+TEST(ConfigFile,AddKey)
+{
+    ConfigFile configFile;
+    configFile.AddKey("key_test","value_test");
+    ASSERT_TRUE(configFile.GetKey("key_test")=="value_test");
+}
+
+TEST(ConfigFile, BadKey)
+{
+    if(filesystem::exists("config.ini")) filesystem::rename("config.ini","config.ini_bak");
+
+    ofstream file("config.ini");
+    file << "THISISABADKEY=44"<<endl;
+    file.close();
+
+    ASSERT_THROW(ConfigFile configFile,runtime_error);
+
+    if(filesystem::exists("config.ini_bak")) filesystem::rename("config.ini_bak","config.ini");
 }
